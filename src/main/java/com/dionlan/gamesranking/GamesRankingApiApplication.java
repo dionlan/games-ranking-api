a1427bb1a43232a5b43464eaefc1e6eda1b6bef9
dionlan <dionlan.alves@gmail.com>
2022-05-05 21:22:57 -0300

    Update deploy.

--- src/main/java/com/dionlan/gamesranking/GamesRankingApiApplication.java
+++ src/main/java/com/dionlan/gamesranking/GamesRankingApiApplication.java
@@ -2,9 +2,10 @@ package com.dionlan.gamesranking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GamesRankingApiApplication {
+public class GamesRankingApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamesRankingApiApplication.class, args);
